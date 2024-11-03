cfde534d663000ce7ef2d62c02a666e935ddc9f9
asma ben salah <asmabensalah468@gmail.com>
2024-11-03 22:49:59 +0100

    js

---
+++ exercice.js
@@ -0,0 +1,67 @@
+
+
+function reverseString(str) {
+   
+    let n=str.length;
+    let ch ="";
+    for (let i=0; i<n ; i++)
+
+    
+    {
+      ch=str[i]+ch
+      
+    }
+    return ch
+}
+    
+
+
+function countChars(string) {
+    let count = 0;
+    
+    for (let i=0 ;i < string.length;i++)
+         {
+         
+            count++;
+        
+            }
+    return count;
+}
+
+function MaxMin (arr){
+let max=0;
+let min=0;
+
+for (let i=0; i<arr.max;i++)
+     if (arr[i] > max) {
+        max = arr[i]
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+
+return maximum - minimum ;
+}
+
+function SumofArray (arr)
+{
+    let n=0
+    for (let i=0 ; i < arr.length ; i++){
+      n=n+arr[i]
+        
+    }
+    return n
+   
+    }
+function factorial (fact)
+
+{
+let n=1
+for (let i=1 ; i<=fact ; i++ ){
+n=n*i
+}
+    return n
+
+
+}
+
